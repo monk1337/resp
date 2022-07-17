2abc97152ae25dcaccab575a734a1b35b7b43446
Monk <ankit.pal@saama.com>
2022-07-17 16:47:50 +0530

    bulkdownload

--- resp/resp.py
+++ resp/resp.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from tqdm import tqdm
 from resp.apis.serp_api import Serp
+from resp.apis.cnnp import connected_papers
 
 
 
@@ -80,3 +81,29 @@ class Resp(object):
         result  = self.engine.google_search(
             f'site:{url} {keyword}', max_pages)
         return result
+
+
+    def all_related_papers(self, query):
+        """download all related papers from two sources"""
+
+        result = []
+        cp = connected_papers()
+        rl_result = cp.download_papers(query, n=1)
+        rl_result["source"] = ["connected_papers"] * len(rl_result)
+        rl_result["keyword"] = [query] * len(rl_result)
+        rl_result = rl_result.loc[:, ~rl_result.columns.str.contains("^Unnamed")]
+        rl_result["title"] = rl_result["title"].str.lower()
+        result.append(rl_result)
+        
+        rl_resultgp = self.engine.get_related_pages(query)
+        rl_resultgp = rl_resultgp[list(rl_resultgp.keys())[0]]
+        rl_resultgp["source"] = ["google_scholar"] * len(rl_resultgp)
+        rl_resultgp["keyword"] = [query] * len(rl_resultgp)
+        rl_resultgp["title"] = rl_resultgp["title"].str.lower()
+        result.append(rl_resultgp)
+
+        df = pd.concat(result)
+        df = df.drop_duplicates("title", keep="last")
+        df = df.drop_duplicates("link", keep="last")
+        df = df[["title", "link", "source", "keyword"]]
+        return df.reset_index(drop=True)
